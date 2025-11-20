090be9b3ee7aee91f528d150c48685679bad3d31
alejandro12m <146794481+alejandro12m@users.noreply.github.com>
2025-11-19 22:26:27 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.WebHost.UseUrls("http://0.0.0.0:8080");
+// Obtener el puerto que asigna Railway
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
